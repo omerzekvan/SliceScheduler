e01a4f3e84a048ca4f123b8ebd8c90c03c9dae14
omerzekvan <40953703+omerzekvan@users.noreply.github.com>
2024-01-13 21:22:14 +0300

    Release'e yönelik fazlalıklar atıldı

--- main.py
+++ main.py
@@ -361,58 +361,26 @@ if __name__ == '__main__':
         db = pgdb.DBConn()
         db.connect()
 
-        with open("results.txt", "a") as file1, open("usage.txt", "a") as file2, open("satisfied.txt", "a") as file3, open(
-                    "guests.txt", "a") as file4, open("timeLine.txt", "a") as file5, open("scores.txt", "a") as file6, open("satisfiedLong.txt", "a") as file7, open(
-                    "diffs.txt", "a") as file8, open("avgDiffs.txt", "a") as file9, open("underutil.txt", "a") as file10:
+        with open("usage.txt", "a") as file2, open("satisfied.txt", "a") as file3, open("timeLine.txt", "a") as file5,  open("underutil.txt", "a") as file10:
 
             #with concurrent.futures.ProcessPoolExecutor() as executor:
-            file1.write("NFavailability = {}. 2 pods are onboard if HA({}) is required else only 1 pod is onboard\n".format(NFavailability, HighAv))
+            
             file2.write("NFavailability = {}. 2 pods are onboard if HA({}) is required else only 1 pod is onboard\n".format(NFavailability, HighAv))
             file3.write("NFavailability = {}. 2 pods are onboard if HA({}) is required else only 1 pod is onboard\n".format(NFavailability, HighAv))
-            file4.write("NFavailability = {}. 2 pods are onboard if HA({}) is required else only 1 pod is onboard\n".format(NFavailability, HighAv))
+            
             file5.write("NFavailability = {}. 2 pods are onboard if HA({}) is required else only 1 pod is onboard\n".format(NFavailability, HighAv))
-            file6.write("NFavailability = {}. 2 pods are onboard if HA({}) is required else only 1 pod is onboard\n".format(NFavailability, HighAv))
-            file7.write("NFavailability = {}. 2 pods are onboard if HA({}) is required else only 1 pod is onboard\n".format(NFavailability, HighAv))
-            file8.write("NFavailability = {}. 2 pods are onboard if HA({}) is required else only 1 pod is onboard\n".format(NFavailability, HighAv))
-            file9.write("NFavailability = {}. 2 pods are onboard if HA({}) is required else only 1 pod is onboard\n".format(NFavailability, HighAv))
+            
             file10.write("NFavailability = {}. 2 pods are onboard if HA({}) is required else only 1 pod is onboard\n".format(NFavailability, HighAv))
-            #     strng = "NFavailability = {}. 2 pods are onboard if HA({}) is required else only 1 pod is onboard\n".format(NFavailability, HighAv)
-            #     p1 = executor.submit(file1.write, "HAslkdfm")
-            #     p2 = executor.submit(file2.write, strng)
-            #     p3 = executor.submit(file3.write, str)
-            #     p4 = executor.submit(file4.write, str)
-            #     p5 = executor.submit(file5.write, str)
-            #     p6 = executor.submit(file6.write, str)
-            #     p7 = executor.submit(file7.write, str)
-            #     p8 = executor.submit(file8.write, str)
-            #     p9 = executor.submit(file9.write, str)
-            #
-            #     p1.result()
-            #     p2.result()
-            #     p3.result()
-            #     p4.result()
-            #     p5.result()
-            #     p6.result()
-            #     p7.result()
-            #     p8.result()
-            #     p9.result()
 
-        controlGroups = 6 #12
+        controlGroups = 6 
         for numberOfReqs in range(20, maxNumberOfReqs+1 , 20):
             outputs = []
-
             sumOfUsage = [0] * controlGroups
-
             sumOfUnderUtil = [0] * controlGroups
-
             sumOfSatisfiedReqs = [0] * controlGroups
-
             sumOfGuestF = [0] * controlGroups
-
             sumOfGuestS = [0] * controlGroups
-
             totalTime = [0] * controlGroups
-
             scores = [0] * controlGroups
 
             avrgUsage = [0] * controlGroups
@@ -428,43 +396,16 @@ if __name__ == '__main__':
             avgThirdDiff = 0
 
             for experiment in range(0,numberOfExperiments):
-                #undoList = []
-                #sort(Slices)
-                #sortedSlices = dict(sorted(sliceRequests.items(), key=lambda item: item[1]))
+
                 generateSliceRequests(numberOfReqs)
 
-                # with open("sliceRequests.txt", "r") as file_in:
-                #     sliceRequests = []
-                #     for line in file_in:
-                #         line = ast.literal_eval(line)
-                #         # print(line)
-                #         sliceRequests.append(line)
-
-                #numberOfRequests = len(sliceRequests)
-
-                maxSatisfiedRequestsInExperiment = 0
-
-                maxSatisfiedRequestsInExperimentAmongFavs = 0
-                bestResultAvrgUtilAmongFavs = 0
-                bestResultNumberOfGuestFunctionsAmongFavs = 0
-                bestResultNumberOfGuestSlicesAmongFavs = 0
-                bestResultDurationAmongFavs = 0
-
-                runnerUpDiff = 0
-                thirdDiff = 0
-
-                #List of best performers in the experiment
-                winners = []
-
-                # 11 control sets are simulated. The 12th model choses the best among 3 favorite models
+                # 6 control sets are simulated. 
                 for control in range(0,controlGroups):
 
                     TServices = []
                     FFunctions = []  # Really needed?
                     resetNodes()
                     satisfiedRequests = 0
-                    numberOfGuestFunctions = 0
-                    numberOfGuestSlices = 0
 
                     # Delete all from Functions
                     db.deleteFunctions(-1)
@@ -495,14 +436,7 @@ if __name__ == '__main__':
 
                     leastCapacityNode = True if control == 3 or control == 5 else False
 
-
-                    #for index, sortedSlice in enumerate(sortedSlices):
-                     #   print("Sorted {}: {}".format(index, sortedSlice))
-
                     for r in sortedSlices:
-
-                        #if totalRemainingCapacity() < 6 and (r['priority'] == 1 or (r['priority'] == 2 and totalUnderutilized < 6)):
-                        #    break
 
                         # Break condition inserted not to lose time to find space if there is no hope
                         if (control <= 1 and totalRemainingCapacity() < 6) or (control > 1 and totalRemainingCapacity() < 6 and totalUnderutilized < 6):
@@ -522,9 +456,6 @@ if __name__ == '__main__':
 
                             if control > 1 and r['priority'] == 2:
                                 for t in TServices:
-                                    # tIndex = TServices.index(t)
-
-                                    #intSet = t.functions.intersection(functionsSet)
 
                                     # If this is the service we are looking for and has enough capacity use it
                                     if set(t.reqFunctions) == set(functionsList):
@@ -534,26 +465,19 @@ if __name__ == '__main__':
                                             foundT = True
                                             numberOfGuestFunctions += 1
                                             isGuest = True
-                                            # TODO: Arrange availability
                                             break
-                                        #elif r['priority'] == 2 and r['availability'] < t.availability and t.replicas-1 > t.guests:
-                                        #elif r['priority'] == 2 and t.replicas - 1 > t.guests:
-                                        #elif r['priority'] == 2 and t.fDeployments[0].residualCPU >= functionsCatalog[functionsList[0]]["cpu"]:
                                         
                                         # t.fDeployments[0] the host slice
                                         elif t.fDeployments[0].residualCPU >= functionsCatalog[functionsList[0]]["cpu"]:
                                             t.guests += 1
                                             foundT = True    
                                         
-                                            numberOfGuestFunctions += 1
                                             totalUnderutilized -= functionsCatalog[functionsList[0]]["cpu"]
                                             isGuest = True
                                             t.fDeployments[0].residualCPU -= functionsCatalog[functionsList[0]]["cpu"]
 
-                                            # TODO: Arrange availability
                                             break
 
-                                 #   sortedServices = sort(Services)
                             if not foundT:
                                 # Assign new t with capacity 5
                                 t = slice.Service(functionsList, 2, r['availability'])
@@ -568,7 +492,6 @@ if __name__ == '__main__':
 
                                     for u in functionsCatalog:
                                         if u["name"] == functionsCatalog[f]["name"]:
-                                            # type = u["name"]
                                             cpu = u["cpu"]
                                             av = u["availability"]
                                             break
@@ -604,7 +527,7 @@ if __name__ == '__main__':
                         if not sliceFailed:
                             db.activateSlice(new_slice_id)
                             satisfiedRequests += 1
-                            if isGuest: numberOfGuestSlices += 1
+                            
                             totalUnderutilized += sliceUnderutilized
 
                     #End time
@@ -628,78 +551,21 @@ if __name__ == '__main__':
                     sumOfUsage[control] += avrgUtil
                     sumOfUnderUtil[control] += underUtil
                     sumOfSatisfiedReqs[control] += satisfiedRequests
-                    sumOfGuestF[control] += numberOfGuestFunctions
-                    sumOfGuestS[control] += numberOfGuestSlices
                     totalTime[control] += duration
 
                     satisfiedLong[control] = satisfiedRequests
 
-                    #If this is the best result so far
-                    if maxSatisfiedRequestsInExperiment <  satisfiedRequests:
-                        winners = [] # Initialize the winners list, we have a new winner candidate
-                        winners.append(control)
-                        maxSatisfiedRequestsInExperiment = satisfiedRequests
-                    elif maxSatisfiedRequestsInExperiment == satisfiedRequests:
-                        winners.append(control)
-
-                    #If this is the best result so far among the chosen models
-                    #if control == 2 or control == 9 or control == 10:
-                    # if control == 1 or control == 2 or control == 3:
-                    #     #Calculating the total duration for chosen models
-                    #     bestResultDurationAmongFavs += duration
-                    #     #totalOfFavs += satisfiedRequests
-                    #
-                    #     if maxSatisfiedRequestsInExperimentAmongFavs < satisfiedRequests:
-                    #         bestResultAvrgUtilAmongFavs = avrgUtil
-                    #         maxSatisfiedRequestsInExperimentAmongFavs = satisfiedRequests
-                    #         bestResultNumberOfGuestFunctionsAmongFavs = numberOfGuestFunctions
-                    #         bestResultNumberOfGuestSlicesAmongFavs = numberOfGuestSlices
-
                     outputs.append("Control Set: {} Total Number of requests: {} Number of satisfied requests: {} Number of guests: {} Average Utilization: {}".format(
                         control, numberOfReqs, satisfiedRequests, numberOfGuestSlices, avrgUtil))
-                    print("Total Number of requests: {} Number of satisfied requests: {}".format(numberOfReqs, satisfiedRequests))
+                    # print("Total Number of requests: {} Number of satisfied requests: {}".format(numberOfReqs, satisfiedRequests))
 
-                # sumOfUsage[controlGroups-1] += bestResultAvrgUtilAmongFavs
-                # sumOfSatisfiedReqs[controlGroups-1] += maxSatisfiedRequestsInExperimentAmongFavs
-                # sumOfGuestF[controlGroups-1] += bestResultNumberOfGuestFunctionsAmongFavs
-                # sumOfGuestS[controlGroups-1] += bestResultNumberOfGuestSlicesAmongFavs
-                # totalTime[controlGroups-1] += bestResultDurationAmongFavs
-
-                # satisfiedLong[controlGroups-1] = maxSatisfiedRequestsInExperimentAmongFavs
-
-                for w in winners:
-                    scores[w] += 1
-
-                # outputs.append("Control Set: {} Total Number of requests: {} Number of satisfied requests: {} Number of guests: {} Average Utilization: {}".format(
-                #                         controlGroups-1, numberOfReqs, maxSatisfiedRequestsInExperimentAmongFavs, bestResultNumberOfGuestSlicesAmongFavs, bestResultAvrgUtilAmongFavs))
-
-
-
-                sortedSatisfiedLong = sort(satisfiedLong)
-                runnerUpDiff = sortedSatisfiedLong[controlGroups-2] - sortedSatisfiedLong[controlGroups-3]
-                thirdDiff = sortedSatisfiedLong[controlGroups-2] - sortedSatisfiedLong[controlGroups-4]
-
-                avgRunnerUpDiff += runnerUpDiff/numberOfExperiments
-                avgThirdDiff += thirdDiff/numberOfExperiments
-
-                with open("satisfiedLong.txt", "a") as file7, open("diffs.txt", "a") as file8:
-
-                    for c in range(0, controlGroups):
-                        file7.write(str(satisfiedLong[c]) + " ")
-
-                    file7.write("\n")
-
-                    file8.write(str(runnerUpDiff) + " " + str(thirdDiff) + "\n")
-
-            with open("results.txt", "a") as file1, open("usage.txt", "a") as file2, open("satisfied.txt", "a") as file3, open("guests.txt", "a") as file4, open(
-                    "timeLine.txt", "a") as file5, open("scores.txt", "a") as file6,  open("satisfiedLong.txt", "a") as file7,  open("diffs.txt", "a") as file8, open(
-                    "avgDiffs.txt", "a") as file9, open("underutil.txt", "a") as file10:
+            
+            with open("usage.txt", "a") as file2, open("satisfied.txt", "a") as file3, open(
+                    "timeLine.txt", "a") as file5, open("underutil.txt", "a") as file10:
 
                 usageLine = ""
                 underUtilLine = ""
                 satisfiedReqsline = ""
-                guestFLine = ""
-                guestSLine = ""
                 timeLine = ""
                 scoreLine = ""
 
@@ -707,55 +573,31 @@ if __name__ == '__main__':
                     avrgUsage[c] = round(sumOfUsage[c] / numberOfExperiments, 2)
                     avrgUnderUtil[c] = round(sumOfUnderUtil[c] / numberOfExperiments, 6)
                     avrgSatisfiedReqs[c] = round(sumOfSatisfiedReqs[c] / numberOfExperiments, 2)
-                    avrgGuestF[c] = round(sumOfGuestF[c] / numberOfExperiments, 2)
-                    avrgGuestS[c] = round(sumOfGuestS[c] / numberOfExperiments, 2)
                     avrgTime[c] = round(totalTime[c] / numberOfExperiments, 4) # / numberOfReqs, 4)
 
                     usageLine += str(avrgUsage[c]) + " "
                     underUtilLine += str(avrgUnderUtil[c]) + " "
                     satisfiedReqsline += str(avrgSatisfiedReqs[c]) + " "
-                    guestFLine +=  str(avrgGuestF[c])  + " "
-                    guestSLine += str(avrgGuestS[c]) + " "
                     timeLine += str(avrgTime[c]) + " "
                     scoreLine += str(scores[c]) + " "
 
                 usageLine += "\n"
                 underUtilLine += "\n"
                 satisfiedReqsline += "\n"
-                guestFLine += "\n"
-                guestSLine += "\n"
                 timeLine += "\n"
                 scoreLine += "\n"
 
                 file2.write(usageLine)
                 file3.write(satisfiedReqsline)
-                file4.write(guestSLine)
                 file5.write(timeLine)
-                file6.write(scoreLine)
-
-                file9.write(str(avgRunnerUpDiff) + " " + str(avgThirdDiff) + "\n")
                 file10.write(underUtilLine)
-
-                for o in outputs:
-                    print(o)
-                    # Writing data to a file
-                    file1.write(o + "\n")
-                    #file1.writelines(L)
                     
     except Exception as e:
         print(e)
 
-    with open("results.txt", "a") as file1, open("usage.txt", "a") as file2, open("satisfied.txt", "a") as file3, open(
-            "guests.txt", "a") as file4, open("timeLine.txt", "a") as file5, open("scores.txt", "a") as file6,  open(
-            "satisfiedLong.txt", "a") as file7,  open("diffs.txt", "a") as file8, open("avgDiffs.txt", "a") as file9, open("underutil.txt", "a") as file10:
+    with open("usage.txt", "a") as file2, open("satisfied.txt", "a") as file3, open("timeLine.txt", "a") as file5, open("underutil.txt", "a") as file10:
 
-        file1.write("\n")
         file2.write("\n")
         file3.write("\n")
-        file4.write("\n")
         file5.write("\n")
-        file6.write("\n")
-        file7.write("\n")
-        file8.write("\n")
-        file9.write("\n")
         file10.write("\n")
